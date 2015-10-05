d895273f62acfa85b2bd3539e52381a979703756
okunishinishi <okunishitaka.com@gmail.com>
2015-10-05 21:46:21 +0900

    Version incremented to 1.0.1

--- ci/build.js
+++ ci/build.js
@@ -9,15 +9,24 @@
 process.chdir(__dirname + '/..');
 
 var apeTasking = require('ape-tasking'),
+    apeCompiling = require('ape-compiling'),
     coz = require('coz');
 
 apeTasking.runTasks('build', [
     function renderBud(callback) {
         coz.render([
             '.*.bud',
-            'example/.*.bud',
+            'doc/**/.*.bud',
+            'example/**/.*.bud',
             'lib/.*.bud',
             'test/.*.bud'
         ], callback);
+    },
+    function browsify(callback) {
+        var src = require.resolve('../lib'),
+            dest = require('../bower')['main'];
+        apeCompiling.renderBrowserScript(src, dest, {
+            as: 'numcal'
+        }, callback);
     }
 ], true);
